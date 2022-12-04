7bc991eab461bbd17080bb3c5ed2f6e2dd566ea3
SophiaSilveira <sophiasilveira2003@gmail.com>
2022-12-04 16:02:03 -0300

    icons map page

--- magnum_chaos/lib/telas/mapPage.dart
+++ magnum_chaos/lib/telas/mapPage.dart
@@ -6,9 +6,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:magnum_chaos/telas/gainsPage.dart';
 import 'package:magnum_chaos/telas/phase_one/introductionPage.dart';
 import 'package:magnum_chaos/telas/phase_zero/zeroPage.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart' show showBarModalBottomSheet;
-
-
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart'
+    show showBarModalBottomSheet;
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -33,22 +32,33 @@ class _MapPageState extends State<MapPage> {
         appBar: AppBar(
           title: Text("Tela Mapa:"),
           automaticallyImplyLeading: false,
-          toolbarHeight: 80,
+          toolbarHeight: 100,
           centerTitle: true,
-          backgroundColor: Colors.black12,//Color(0xff0A6D92),
+          backgroundColor: Colors.black12, //Color(0xff0A6D92),
           actions: [
             IconButton(
-              icon: const Icon(
+              icon: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/logo/logoRedondo.png'),
+              ),
+              iconSize: 65,
+
+              //color: Colors.white,
+              /*const Icon(
                 Icons.account_circle_rounded,
                 color: Colors.white,
-              ),
+              ),*/
               onPressed: () {
-                showBarModalBottomSheet(context: context, builder: (context) => GainsPage());
+                showBarModalBottomSheet(
+                    context: context, builder: (context) => GainsPage());
               },
             ),
           ],
         ),
-        body: fases(context),
+        body: Container(
+          alignment: Alignment.center,
+          child: fases(context),
+          ), 
       ),
     );
   }
@@ -56,38 +66,63 @@ class _MapPageState extends State<MapPage> {
   Column fases(BuildContext context) {
     bool _isButtonDisabled = false;
     return Column(
-        children: [       
-          Container(
-            height: MediaQuery.of(context).size.height * 0.3,
-            margin: EdgeInsets.only(top: 80, left: 100),
-            child: ElevatedButton(
-              child: Text("Ir para a tela da fase zero"),
-              onPressed: (){
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: ((context) => ZeroPage()))
-                );
-              },
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 25, right: 200),
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 0.25,
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: ((context) => const ZeroPage())));
+            },
+            icon: CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('assets/image/portal_preto.png'),
+              backgroundColor: Colors.transparent,
             ),
           ),
-          Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          margin: EdgeInsets.only(top: 80, right: 80),
-            child: ElevatedButton(
-              child: _isButtonDisabled ? null : const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/logo/logoRedondo.png'),
-              ),
-              onPressed: _isButtonDisabled ? null : (){
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: ((context) => const IntroductonPage()))
-                );
-              },
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 20,left: 200),
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 0.25,
+          child: _isButtonDisabled ? null : IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: ((context) => const IntroductonPage())));
+            },
+            icon: CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('assets/image/portal_quadrado.png'),
+              backgroundColor: Colors.transparent,
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
+
+/*ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.transparent,
+              onPrimary: Colors.transparent,
+              
+            ),
+            onPressed: (){
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: ((context) => const IntroductonPage()))
+              );
+            },
+            child: const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/logo/logoRedondo_700.png'),
+            ),
+          ),*/
 
